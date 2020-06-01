549ca94142ad8897bf95aedbc72e917bd3afef4a
jiaxiyang <jiaxiyangbit@126.com>
2020-06-01 15:50:04 +0800

    cancel eglot hightlight

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 (setq make-backup-files nil)
 (setq split-width-threshold 0)
 (setq split-height-threshold nil)
+(setq eglot-ignored-server-capabilites '(:documentHighlightProvider)) ;; eglot不高亮
 
 (eval-when-compile
   ;; no-load-path.el must be found in `load-path`. Fortunately this is
