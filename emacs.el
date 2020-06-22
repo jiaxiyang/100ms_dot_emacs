00a036e78d563a9711cfd6f360c3b30ac62e0ed9
Jiadakong1 <Jiadakong1@github.com>
2020-06-22 23:21:51 +0800

    add emacs.el

--- emacs.el
+++ emacs.el
@@ -2,4 +2,33 @@
 (setq make-backup-files nil)
 (setq split-width-threshold 0)
 (setq split-height-threshold nil)
-(setq eglot-ignored-server-capabilites '(:documentHighlightProvider)) ;; eglot不高亮
+(setq eglot-ignored-server-capabilites '(:documentHighlightProvider))
+;; eglot不高亮
+
+
+;; ibuffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq ibuffer-saved-filter-groups
+      (quote (("default"
+               ("dired" (mode . dired-mode))
+               ("c++" (mode . c++-mode))
+               ("python" (mode . python-mode))
+               ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
+               ("org" (name . "^.*org$"))
+               ("magit" (mode . magit-mode))
+               ("emacs" (or
+                         (name . "^\\*scratch\\*$")
+                         (name . "^\\*Messages\\*$")))
+               ))))
+(add-hook 'ibuffer-mode-hook
+          (lambda ()
+            (ibuffer-auto-mode 1)
+            (ibuffer-switch-to-saved-filter-groups "default")))
+
+;; don't show these
+                                        ;(add-to-list 'ibuffer-never-show-predicates "zowie")
+;; Don't show filter groups if there are no buffers in that group
+(setq ibuffer-show-empty-filter-groups nil)
+
+;; Don't ask for confirmation to delete marked buffers
+(setq ibuffer-expert t)
