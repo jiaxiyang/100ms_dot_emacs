68db206b6be55672254c976e2c22166c9cde95b2
jiaxiyang <jiaxiyangbit@163.com>
2020-09-13 12:12:56 +0800

    update

--- emacs.el
+++ emacs.el
@@ -52,3 +52,20 @@
 
 ;; Don't ask for confirmation to delete marked buffers
 (setq ibuffer-expert t)
+
+
+
+;; (transient-mark-mode t)
+(defadvice kill-ring-save (before slick-copy activate compile)
+ "When called interactively with no active region, copy a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (message"Copied line")
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
+(defadvice kill-region (before slick-cut activate compile)
+ "When called interactively with no active region, kill a single line instead."
+  (interactive
+   (if mark-active (list (region-beginning) (region-end))
+     (list (line-beginning-position)
+           (line-beginning-position 2)))))
