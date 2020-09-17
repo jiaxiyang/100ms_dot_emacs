2e8c42de576e7ef143483c59f013472cd3ff2133
jiaxiyang <jiaxiyangbit@126.com>
2020-09-17 15:15:04 +0800

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -108,6 +108,8 @@
   (f-switch-to-buffer 0))
 (global-set-key (kbd "M-j") 'c-switch-to-prev-buffer)
 (global-set-key (kbd "M-k") 'c-switch-to-next-buffer)
-(global-set-key (kbd "M-j") 'awesome-tab-backward-tab)
-(global-set-key (kbd "M-k") 'awesome-tab-forward-tab)
+(global-set-key (kbd "M-h") 'awesome-tab-backward-tab)
+(global-set-key (kbd "M-j") 'awesome-tab-forward-group)
+(global-set-key (kbd "M-k") 'awesome-tab-backward-group)
+(global-set-key (kbd "M-l") 'awesome-tab-forward-tab)
 
