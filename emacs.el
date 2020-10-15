d12455d50730ac04252be6e99a8a00a61049d1ca
jiaxiyang <jiaxiyangbit@126.com>
2020-10-15 11:38:35 +0800

    Update emacs.el

--- emacs.el
+++ emacs.el
@@ -89,6 +89,7 @@
             (local-set-key (kbd "M-h") #'awesome-tab-backward-tab)
             (local-set-key (kbd "C-M-j") #'org-meta-return)))
 (setq-default org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3))
+(setq org-clock-out-remove-zero-time-clocks t)
 (setq org-startup-folded t
       org-agenda-span 'day
       org-agenda-window-setup 'current-window
