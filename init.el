277cb6cee8f6e156089ecfbf05bd9543fbd1cc50
jiaxiyang <jiaxiyangbit@126.com>
2021-01-12 17:31:58 +0800

    Update init.el

--- init.el
+++ init.el
@@ -611,7 +611,12 @@
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
   (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-  (setq company-tooltip-align-annotations t))
+  (setq racer-rust-src-path
+              (concat (string-trim
+                       (shell-command-to-string "rustc --print sysroot"))
+                      "/lib/rustlib/src/rust/library"))
+  (setq company-tooltip-align-annotations t)
+  (setq racer-loaded 1))
 
 ;; == gcmh
 (use-package gcmh
